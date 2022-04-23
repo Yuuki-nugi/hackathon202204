53ef85a772bd15bd9d9f6884ced7a2ae4839146e
itsu308 <itsuki.s625@gmail.com>
2022-04-23 14:21:24 +0900

    application_controller周りの実装と環境変数

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
+require 'line/bot'
+
 class ApplicationController < ActionController::API
+
+    before_action :validate_singnature, except: [:new, :create]
+    def validate_singnature
+        body = request.body.read
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+            render json: { status: 400, message: 'Bad Request'}
+        end
+    end
+
+    def client
+        @client ||= Line::Bot::Client.new { |config|
+          config.channel_secret = ENV['LINE_API_CHANNEL_SECRET']
+          config.channel_token = ENV['LINE_API_CHANNEL_TOKEN']
+    }
+    end
+
 end
